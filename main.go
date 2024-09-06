e0edea4ff18e9d7b6cbc874babc0cb0abfd3d055
Brian Ott <bott@b1-systems.de>
2024-09-06 15:15:28 +0200

    update: implemented a middleware for metrics handler

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+    "fmt"
     "log"
     "net/http"
 )
 
-type Dir string
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
     const filepathRoot = "."
     const port = "8080"
 
+    apiCfg := apiConfig {
+        fileserverHits: 0,
+    }
+
     mux := http.NewServeMux()
-    mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+    mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
     mux.HandleFunc("/healthz", handlerReadiness)
+    mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+    mux.HandleFunc("/reset", apiCfg.handlerReset)
 
     srv := &http.Server {
         Addr: ":" + port,
@@ -24,9 +33,15 @@ func main() {
     log.Fatal(srv.ListenAndServe())
 }
 
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-        w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-        w.WriteHeader(http.StatusOK)
-        w.Write([]byte(http.StatusText(http.StatusOK)))
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits++
+        next.ServeHTTP(w, r)
+    })
 }
