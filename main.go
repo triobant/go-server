395ba472687cf8fd61752625104c03e240b34e52
Brian Ott <bott@b1-systems.de>
2024-09-06 12:13:45 +0200

    update: main and add a custom handler

--- main.go
+++ main.go
@@ -12,14 +12,21 @@ func main() {
     const port = "8080"
 
     mux := http.NewServeMux()
-    mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+    mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+    mux.HandleFunc("/healthz", handlerReadiness)
 
     srv := &http.Server {
         Addr: ":" + port,
         Handler: mux,
     }
 
-
     log.Printf("Serving files from %s onport: %s\n", filepathRoot, port)
     log.Fatal(srv.ListenAndServe())
+}
+
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+        w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte(http.StatusText(http.StatusOK)))
 }
