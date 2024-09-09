d26528cd18de060548eda9a618789b0c41e2a0b9
Brian Ott <bott@b1-systems.de>
2024-09-09 10:15:48 +0200

    update: paths to /api

--- main.go
+++ main.go
@@ -20,9 +20,9 @@ func main() {
 
     mux := http.NewServeMux()
     mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-    mux.HandleFunc("GET /healthz", handlerReadiness)
-    mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-    mux.HandleFunc("/reset", apiCfg.handlerReset)
+    mux.HandleFunc("GET /api/healthz", handlerReadiness)
+    mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+    mux.HandleFunc("/api/reset", apiCfg.handlerReset)
 
     srv := &http.Server {
         Addr: ":" + port,
