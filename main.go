f7837d0bf7da191478e930be60160c75ef028229
Brian Ott <bott@b1-systems.de>
2024-09-09 12:09:27 +0200

    update: json encode and decode the POST request on /api/validate_chirp

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "encoding/json"
     "log"
     "net/http"
 )
@@ -21,10 +22,13 @@ func main() {
     fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
     mux.Handle("/app/", fsHandler)
 
+
     mux.HandleFunc("GET /api/healthz", handlerReadiness)
     mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
+    mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
 
     mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+
 
     srv := &http.Server {
         Addr: ":" + port,
@@ -33,4 +37,55 @@ func main() {
 
     log.Printf("Serving files from %s onport: %s\n", filepathRoot, port)
     log.Fatal(srv.ListenAndServe())
+}
+
+func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+    type parameters struct {
+        Body string `json:"body"`
+    }
+    type returnVals struct {
+        Valid bool `json:"valid"`
+    }
+
+    decoder := json.NewDecoder(r.Body)
+    params := parameters{}
+    err := decoder.Decode(&params)
+    if err != nil {
+        respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+        return
+    }
+
+    const maxChirpLength = 140
+    if len(params.Body) > maxChirpLength {
+        respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+        return
+    }
+
+    respondWithJSON(w, http.StatusOK, returnVals{
+        Valid: true,
+    })
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+    if code > 499 {
+        log.Printf("Responding with 5XX error: %s", msg)
+    }
+    type errorResponse struct {
+        Error string `json:"error"`
+    }
+    respondWithJSON(w, code, errorResponse{
+        Error: msg,
+    })
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+    w.Header().Set("Content-Type", "application/json")
+    dat, err := json.Marshal(payload)
+    if err != nil {
+        log.Printf("Error marshalling JSON: %s", err)
+        w.WriteHeader(500)
+        return
+    }
+    w.WriteHeader(code)
+    w.Write(dat)
 }
