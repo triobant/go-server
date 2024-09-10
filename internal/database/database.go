158b86410ac1ffa4c0aa451586228f2a95ef6b1d
Brian Ott <bott@b1-systems.de>
2024-09-10 11:41:57 +0200

    update: some comments were added

--- internal/database/database.go
+++ internal/database/database.go
@@ -21,6 +21,8 @@ type Chirp struct {
 	Body string `json:"body"`
 }
 
+// NewDB creates a new database connection
+// and creates the database file if it doesn't exist
 func NewDB(path string) (*DB, error) {
     db := &DB{
         path:   path,
@@ -30,6 +32,7 @@ func NewDB(path string) (*DB, error) {
     return db, err
 }
 
+// CreateChirp creates a new chirp and saves it to disk
 func (db *DB) CreateChirp(body string) (Chirp, error) {
     dbStructure, err := db.loadDB()
     if err != nil {
@@ -51,6 +54,7 @@ func (db *DB) CreateChirp(body string) (Chirp, error) {
     return chirp, nil
 }
 
+// GetChirps returns all chirps in the database
 func (db *DB) GetChirps() ([]Chirp, error) {
     dbStructure, err := db.loadDB()
     if err != nil {
@@ -72,6 +76,7 @@ func (db *DB) createDB() error {
     return db.writeDB(dbStructure)
 }
 
+// ensureDB creates a new database file if it doesn't exist
 func (db *DB) ensureDB() error {
     _, err := os.ReadFile(db.path)
     if errors.Is(err, os.ErrNotExist) {
@@ -80,6 +85,7 @@ func (db *DB) ensureDB() error {
     return err
 }
 
+// loadDB reads the database file into memory
 func (db *DB) loadDB() (DBStructure, error) {
     db.mu.RLock()
     defer db.mu.RUnlock()
@@ -97,6 +103,7 @@ func (db *DB) loadDB() (DBStructure, error) {
     return dbStructure, nil
 }
 
+// writeDB writes the database file to disk
 func (db *DB) writeDB(dbStructure DBStructure) error {
     db.mu.Lock()
     defer db.mu.Unlock()
