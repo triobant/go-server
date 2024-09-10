2283cae0869ec2a2ac1b43e33f2f8dc8982ac4e6
Brian Ott <bott@b1-systems.de>
2024-09-10 11:38:11 +0200

    update: moved some funcs to seperate files and adjusting the structs

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-    "encoding/json"
     "log"
     "net/http"
-    "strings"
+    "github.com/triobant/go-server/internal/database"
 )
 
 type apiConfig struct {
-	fileserverHits int
+	fileserverHits  int
+    DB              *database.DB
 }
 
 func main() {
     const filepathRoot = "."
     const port = "8080"
 
+    db, err := database.NewDB("database.json")
+    if err != nil {
+        log.Fatal(err)
+    }
     apiCfg := apiConfig {
         fileserverHits: 0,
+        DB:             db,
     }
 
     mux := http.NewServeMux()
@@ -26,7 +31,8 @@ func main() {
 
     mux.HandleFunc("GET /api/healthz", handlerReadiness)
     mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
-    mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+    mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+    mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
 
     mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
@@ -38,74 +44,4 @@ func main() {
 
     log.Printf("Serving files from %s onport: %s\n", filepathRoot, port)
     log.Fatal(srv.ListenAndServe())
-}
-
-func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
-    type parameters struct {
-        Body string `json:"body"`
-    }
-    type returnVals struct {
-        CleanedBody string `json:"cleaned_body"`
-    }
-
-    decoder := json.NewDecoder(r.Body)
-    params := parameters{}
-    err := decoder.Decode(&params)
-    if err != nil {
-        respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
-        return
-    }
-
-    const maxChirpLength = 140
-    if len(params.Body) > maxChirpLength {
-        respondWithError(w, http.StatusBadRequest, "Chirp is too long")
-        return
-    }
-
-    badWords := map[string]struct{}{
-        "kerfuffle": {},
-        "sharbert": {},
-        "fornax": {},
-    }
-    cleaned := getCleanedBody(params.Body, badWords)
-
-    respondWithJSON(w, http.StatusOK, returnVals{
-        CleanedBody: cleaned,
-    })
-}
-
-func getCleanedBody(body string, badWords map[string]struct{}) string {
-    words := strings.Split(body, " ")
-    for i, word := range words {
-        loweredWord := strings.ToLower(word)
-        if _, ok := badWords[loweredWord]; ok {
-            words[i] = "****"
-        }
-    }
-    cleaned := strings.Join(words, " ")
-    return cleaned
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-    if code > 499 {
-        log.Printf("Responding with 5XX error: %s", msg)
-    }
-    type errorResponse struct {
-        Error string `json:"error"`
-    }
-    respondWithJSON(w, code, errorResponse{
-        Error: msg,
-    })
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-    w.Header().Set("Content-Type", "application/json")
-    dat, err := json.Marshal(payload)
-    if err != nil {
-        log.Printf("Error marshalling JSON: %s", err)
-        w.WriteHeader(500)
-        return
-    }
-    w.WriteHeader(code)
-    w.Write(dat)
 }
