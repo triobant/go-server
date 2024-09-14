2e3139f8c4056016472fd683deb2db4f996b4222
Brian Ott <bott@b1-systems.de>
2024-09-14 23:37:38 +0200

    update: registered new route to update users

--- main.go
+++ main.go
@@ -45,6 +45,7 @@ func main() {
 
     mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
     mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
+    mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
 
     mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
     mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
