ec7ac9d68b40bf3cbfe64d20639ce0d75ad82b2c
Brian Ott <bott@b1-systems.de>
2024-09-18 21:22:07 +0200

    update: login handler to add default expiration for token and make jwt; returns Token with json

--- handler_login.go
+++ handler_login.go
@@ -3,17 +3,20 @@ package main
 import (
     "encoding/json"
     "net/http"
+    "time"
 
     "github.com/triobant/go-server/internal/auth"
 )
 
 func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
     type parameters struct {
-        Password    string  `json:"password"`
-        Email       string  `json:"email"`
+        Password            string  `json:"password"`
+        Email               string  `json:"email"`
+        ExpiresInSeconds    int     `json:"expires_in_seconds"`
     }
     type response struct {
         User
+        Token string `json:"token"`
     }
 
     decoder := json.NewDecoder(r.Body)
@@ -36,10 +39,24 @@ func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+    defaultExpiration := 60 * 60 * 24
+    if params.ExpiresInSeconds == 0 {
+        params.ExpiresInSeconds = defaultExpiration
+    } else if params.ExpiresInSeconds > defaultExpiration {
+        params.ExpiresInSeconds = defaultExpiration
+    }
+
+    token, err := auth.MakeJWT(user.ID, cfg.jwtSecret, time.Duration(params.ExpiresInSeconds)*time.Second)
+    if err != nil {
+        respondWithError(w, http.StatusInternalServerError, "Couldn't create JWT")
+        return
+    }
+
     respondWithJSON(w, http.StatusOK, response{
         User: User{
-            ID:     user.ID,
-            Email:  user.Email,
+            ID:             user.ID,
+            Email:          user.Email,
         },
+        Token: token,
     })
 }
