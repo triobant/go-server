5e5cef4bc2a9632e559b48d80c2d2c545c6627c6
Brian Ott <bott@b1-systems.de>
2024-09-22 23:17:03 +0200

    update: func to save refreshed token[s] in db

--- internal/database/refresh_tokens.go
+++ internal/database/refresh_tokens.go
@@ -9,3 +9,23 @@ type RefreshToken struct {
     Token       string      `json:"token"`
     ExpiresAt   time.Time   `json:"expires_at"`
 }
+
+func (db *DB) SaveRefreshToken(userID int, token string) error {
+    dbStructure, err := db.loadDB()
+    if err != nil {
+        return err
+    }
+
+    refreshToken := RefreshToken{
+        UserID:         userID,
+        Token:          token,
+        ExpiresAt:      time.Now().Add(time.Hour),
+    }
+    dbStructure.RefreshTokens[token] = refreshToken
+
+    err = db.writeDB(dbStructure)
+    if err != nil {
+        return err
+    }
+    return nil
+}
