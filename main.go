51136107c8057a47753ebf8ffd5e150271a18955
Brian Ott <bott@b1-systems.de>
2024-11-07 09:43:51 +0100

    update: migration to psql

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-    "flag"
+    "database/sql"
     "log"
     "net/http"
     "os"
+    "sync/atomic"
 
     "github.com/triobant/go-server/internal/database"
     "github.com/joho/godotenv"
+    _ "github.com/lib/pq"
 )
 
 type apiConfig struct {
-	fileserverHits  int
-    DB              *database.DB
+	fileserverHits  atomic.Int32
+    db              *database.Queries
+    platform        string
     jwtSecret       string
 }
 
@@ -24,29 +27,29 @@ func main() {
     if err != nil {
         log.Fatal("Error loading .env file")
     }
-
+    dbURL := os.Getenv("DB_URL")
+    if dbURL == "" {
+        log.Fatal("DB_URL must be set")
+    }
+    platform := os.Getenv("PLATFORM")
+    if platform == "" {
+        log.Fatal("PLATFORM must be set")
+    }
     jwtSecret := os.Getenv("JWT_SECRET")
     if jwtSecret == "" {
         log.Fatal("JWT_SECRET environment variable is not set")
     }
 
-    db, err := database.NewDB("database.json")
+    dbConn, err := sql.Open("postgres", dbURL)
     if err != nil {
-        log.Fatal(err)
+        log.Fatal("Error opening database: %s", err)
     }
-
-    dbg := flag.Bool("debug", false, "Enable debug mode")
-    flag.Parse()
-    if dbg != nil && *dbg {
-        err := db.ResetDB()
-        if err != nil {
-            log.Fatal(err)
-        }
-    }
+    dbQueries := database.New(dbConn)
 
     apiCfg := apiConfig {
-        fileserverHits: 0,
-        DB:             db,
+        fileserverHits: atomic.Int32{},
+        db:             dbQueries,
+        platform:       platform,
         jwtSecret:      jwtSecret,
     }
 
@@ -55,28 +58,27 @@ func main() {
     mux.Handle("/app/", fsHandler)
 
     mux.HandleFunc("GET /api/healthz", handlerReadiness)
-    mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
 
-    mux.HandleFunc("POST /api/login", apiCfg.handlerLogin) // TODO: update login handler
+    mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
+    mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
+    mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
 
     mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
-    mux.HandleFunc("PUT /api/users", apiCfg.handlerUsersUpdate)
 
     mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
     mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
     mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
 
-    mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
-    mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
 
+    mux.HandleFunc("GET /admin/reset", apiCfg.handlerReset)
     mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 
 
     srv := &http.Server {
-        Addr: ":" + port,
-        Handler: mux,
+        Addr:       ":" + port,
+        Handler:    mux,
     }
 
-    log.Printf("Serving files from %s onport: %s\n", filepathRoot, port)
+    log.Printf("Serving on port: %s\n", port)
     log.Fatal(srv.ListenAndServe())
 }
