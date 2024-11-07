0cc2f7323734bb9f39ff9c98b4aed50e24e4696a
Brian Ott <bott@b1-systems.de>
2024-11-07 09:42:43 +0100

    update: chirps create to accomodate the migration to postgresql

--- handler_chirps_create.go
+++ handler_chirps_create.go
@@ -5,42 +5,67 @@ import (
     "errors"
     "net/http"
     "strings"
+    "time"
+
+    "github.com/triobant/go-server/internal/auth"
+    "github.com/triobant/go-server/internal/database"
+    "github.com/google/uuid"
 )
 
 type Chirp struct {
-    ID      int     `json:"id"`
-    Body    string  `json:"body"`
+    ID          uuid.UUID   `json:"id"`
+    CreatedAt   time.Time   `json:"created_at"`
+    UpdatedAt   time.Time   `json:"updated_at"`
+    UserID      uuid.UUID   `json:"user_id"`
+    Body        string      `json:"body"`
 
 }
 
 func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request) {
     type parameters struct {
-        Body string `json:"body"`
+        Body    string `json:"body"`
+    }
+
+    token, err := auth.GetBearerToken(r.Header)
+    if err != nil {
+        respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+        return
+    }
+    userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+    if err != nil {
+        respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+        return
     }
 
     decoder := json.NewDecoder(r.Body)
     params := parameters{}
-    err := decoder.Decode(&params)
+    err = decoder.Decode(&params)
     if err != nil {
-        respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+        respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
         return
     }
 
     cleaned, err := validateChirp(params.Body)
     if err != nil {
-        respondWithError(w, http.StatusBadRequest, err.Error())
+        respondWithError(w, http.StatusBadRequest, err.Error(), err)
         return
     }
 
-    chirp, err := cfg.DB.CreateChirp(cleaned)
+    chirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
+        UserID: userID,
+        Body:   cleaned,
+    })
     if err != nil {
-        respondWithError(w, http.StatusInternalServerError, "Couldn't create chirp")
+        respondWithError(w, http.StatusInternalServerError, "Couldn't create chirp", err)
         return
     }
 
     respondWithJSON(w, http.StatusCreated, Chirp{
-        ID:     chirp.ID,
-        Body:   chirp.Body,
+        ID:         chirp.ID,
+        CreatedAt:  chirp.CreatedAt,
+        UpdatedAt:  chirp.UpdatedAt,
+        UserID:     chirp.UserID,
+        Body:       chirp.Body,
     })
 }
 
