87eab4badbf6b441653d84f62dce01989878dbaa
Brian Ott <bott@b1-systems.de>
2024-11-08 15:02:33 +0100

    update: new query to update users

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -12,3 +12,8 @@ RETURNING *;
 -- name: GetUserByEmail :one
 SELECT * FROM users
 WHERE email = $1;
+
+-- name: UpdateUser :one
+UPDATE users SET email = $2, hashed_password = $3, updated_at = NOW()
+WHERE id = $1
+RETURNING *;
