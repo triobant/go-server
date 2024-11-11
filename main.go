b91cbd11f886436882be2f6fa082fcaa08cd5fd6
Brian Ott <bott@b1-systems.de>
2024-11-11 14:41:37 +0100

    update: main to register new route for delete handler

--- main.go
+++ main.go
@@ -69,6 +69,7 @@ func main() {
     mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
     mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
     mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
+    mux.HandleFunc("DELETE /api/chirps/{chirpID}", apiCfg.handlerChirpsDelete)
 
 
     mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
