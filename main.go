24d05da1c28ea571f7e7974eab9675c9c6c4e2e7
Brian Ott <bott@b1-systems.de>
2024-11-14 11:33:12 +0100

    update: new route registered

--- main.go
+++ main.go
@@ -59,6 +59,8 @@ func main() {
 
     mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
+    mux.HandleFunc("POST /api/polka/webhooks", apiCfg.handlerWebhook)
+
     mux.HandleFunc("POST /api/login", apiCfg.handlerLogin)
     mux.HandleFunc("POST /api/refresh", apiCfg.handlerRefresh)
     mux.HandleFunc("POST /api/revoke", apiCfg.handlerRevoke)
@@ -70,8 +72,6 @@ func main() {
     mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
     mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerChirpsGet)
     mux.HandleFunc("DELETE /api/chirps/{chirpID}", apiCfg.handlerChirpsDelete)
-
-    mux.HandleFunc("POST /api/polka/webhooks", apiCfg.handlerPolkaWebhook)
 
     mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
     mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
