e79685ef0d9e6a3133dec19affd373c608008319
Brian Ott <bott@b1-systems.de>
2024-11-18 12:21:10 +0100

    update: handler to optionally sort desc or default to asc

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "net/http"
+    "sort"
 
     "github.com/google/uuid"
 )
@@ -45,6 +46,13 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
             return
         }
     }
+
+    sortDirection := "asc"
+    sortDirectionParam := r.URL.Query().Get("sort")
+    if sortDirectionParam == "desc" {
+        sortDirection = "desc"
+    }
+
     chirps := []Chirp{}
     for _, dbChirp := range dbChirps {
         if authorID != uuid.Nil && dbChirp.UserID != authorID {
@@ -58,6 +66,13 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
             Body:   dbChirp.Body,
         })
     }
+
+    sort.Slice(chirps, func(i, j int) bool {
+        if sortDirection == "desc" {
+            return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
+        }
+        return chirps[i].CreatedAt.Before(chirps[j].CreatedAt)
+    })
 
     respondWithJSON(w, http.StatusOK, chirps)
 }
